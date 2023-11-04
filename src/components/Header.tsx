072657cb29b3eb4ff892e89ee9fe810939b14428
Priyanshu Dhawan <priyanshudhawan55@gmail.com>
2023-11-04 19:16:18 +0530

    tailwind setup

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const Header: React.FC = () => {
+  return (
+    <header className="h-50 w-50">
+      <div className="h-50 text-red-500">Hello From Tailwind</div>
+    </header>
+  )
+}
+
+export default Header
